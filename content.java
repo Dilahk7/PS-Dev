b395f5748d9a7e0c56ada222b4554fedc7289242
Khalid.najam <khalidnajam7@gmail.com>
2022-06-29 08:57:59 +0000

    add code in files types: .java,.xml,.properties remove code in files types: .java,.xml,.properties

--- content.java
+++ content.java
@@ -229,6 +229,6 @@ public class Cadyof {
         System.out.println("ckowi");
         System.out.println("fyklx");
         System.out.println("hbbig");
-        System.out.println("hyvvy");
+        System.out.println("pfilh");
     }
 }
