40eec017275c4c70bb0261b5fc2b7106ffdee47d
Dilahk7 <khalidnajam7@gmail.com>
2022-09-14 14:06:26 +0000

    add code in files types: .java remove code in files types: .iml,.java

--- content.java
+++ content.java
@@ -155,6 +155,6 @@ public class Cadyof {
         System.out.println("afapn");
         System.out.println("wjrwy");
         System.out.println("ogxtr");
-        System.out.println("xbjex");
+        System.out.println("djmhv");
     }
 }
