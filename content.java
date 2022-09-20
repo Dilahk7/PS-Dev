fb66d88b50dc654dde12be4e366d2752920c1c7b
Dilahk7 <khalidnajam7@gmail.com>
2022-09-20 10:57:23 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -93,6 +93,6 @@ public class Cadyof {
         System.out.println("xogut");
         System.out.println("hbwhu");
         System.out.println("ncgfl");
-        System.out.println("ontiq");
+        System.out.println("tccsb");
     }
 }
