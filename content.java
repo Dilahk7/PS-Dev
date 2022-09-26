8c5f8a20f017bfea02a9dacac7166dd939bbff58
Dilahk7 <khalidnajam7@gmail.com>
2022-09-26 08:12:40 +0000

    add code in files types: .html,.ts,.java remove code in files types: .html,.java

--- content.java
+++ content.java
@@ -131,6 +131,6 @@ public class Cadyof {
         System.out.println("xcxrc");
         System.out.println("peaeg");
         System.out.println("rjlid");
-        System.out.println("fxabc");
+        System.out.println("dinyf");
     }
 }
