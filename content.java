171b0ff3404478ac83cfeb0873f5fe10830d3d0f
Khalid Najam <khalid.najam@peoplestrong.com>
2022-11-28 15:15:52 +0000

    add code in files types: .ts,.html,.json,.java,.xhtml remove code in files types: .ts,.html,.json,.java,.xhtml

--- content.java
+++ content.java
@@ -45,6 +45,6 @@ public class Cadyof {
         System.out.println("iypgq");
         System.out.println("mqubw");
         System.out.println("dbmpb");
-        System.out.println("bcann");
+        System.out.println("domnx");
     }
 }
