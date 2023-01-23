59b1a8b27e3d7fa3b68de386a4a710395a68e1df
Dilahk7 <khalidnajam7@gmail.com>
2023-01-23 12:32:26 +0000

    add code in files types: .java,.xhtml remove code in files types: .java,.xhtml

--- content.java
+++ content.java
@@ -78,6 +78,6 @@ public class Cadyof {
         System.out.println("ummic");
         System.out.println("bpfmr");
         System.out.println("mdspi");
-        System.out.println("aitan");
+        System.out.println("xomxh");
     }
 }
