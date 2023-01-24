2215f06073112bcf44d830b3c06a4035d6d75eb6
Dilahk7 <khalidnajam7@gmail.com>
2023-01-24 09:15:26 +0000

    add code in files types: .java,.css,.html,.xhtml remove code in files types: .java,.css,.html,.xhtml

--- content.java
+++ content.java
@@ -78,6 +78,6 @@ public class Cadyof {
         System.out.println("ummic");
         System.out.println("bpfmr");
         System.out.println("mdspi");
-        System.out.println("olrnu");
+        System.out.println("habej");
     }
 }
