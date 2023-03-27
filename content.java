a0785fe19dc91db2b29dcd629cf940d2c7c79129
Khalid.najam <khalidnajam7@gmail.com>
2023-03-27 07:07:27 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -234,6 +234,6 @@ public class Cadyof {
         System.out.println("iapwo");
         System.out.println("enyqa");
         System.out.println("qovkh");
-        System.out.println("xwsor");
+        System.out.println("xnuai");
     }
 }
