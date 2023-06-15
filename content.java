2368e98cf878d2f5ae2df132868c94ed0df0e49f
Khalid Najam <khalid.najam@peoplestrong.com>
2023-06-15 07:14:42 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -22,6 +22,6 @@ public class Cadyof {
         System.out.println("pvbog");
         System.out.println("qlrus");
         System.out.println("atkcb");
-        System.out.println("qnktb");
+        System.out.println("yvfsh");
     }
 }
