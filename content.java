e2ca59498951adb0134923187964fd8c582e38cd
Dilahk7 <khalidnajam7@gmail.com>
2023-06-27 08:57:59 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -112,6 +112,6 @@ public class Cadyof {
         System.out.println("abgkt");
         System.out.println("qmsyc");
         System.out.println("ccwfm");
-        System.out.println("gexca");
+        System.out.println("kxkhp");
     }
 }
