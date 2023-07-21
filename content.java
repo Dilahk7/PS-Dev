60ddd4ec9a1afa74d8b658ec983dbc351100e8fd
Dilahk7 <khalidnajam7@gmail.com>
2023-07-21 13:29:10 +0000

    add code in files types: .java,.properties remove code in files types: .java,.properties

--- content.java
+++ content.java
@@ -122,6 +122,6 @@ public class Cadyof {
         System.out.println("uyisq");
         System.out.println("lgkae");
         System.out.println("qotjb");
-        System.out.println("qlmbo");
+        System.out.println("qucrp");
     }
 }
