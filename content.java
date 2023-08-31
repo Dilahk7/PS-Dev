8e56b2aa55191feafd4b790c5e35c1b11034b3b8
Khalid.najam <khalidnajam7@gmail.com>
2023-08-31 10:28:09 +0000

    add code in files types: .java,.xhtml remove code in files types: .java,.xhtml

--- content.java
+++ content.java
@@ -215,6 +215,6 @@ public class Cadyof {
         System.out.println("rqgps");
         System.out.println("gqbxg");
         System.out.println("fyjvi");
-        System.out.println("wrjmp");
+        System.out.println("tlwus");
     }
 }
